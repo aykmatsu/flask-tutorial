8b08927903458f7be40cf47f84329f01ea1999ca
aykmatsu <toda2020-014@iniad.org>
2021-06-16 04:12:16 +0000

    import random追加

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    uranai = random.choice(['大吉','中吉','小吉'])
+    uranai =random.choice(['大吉','中吉','小吉'])
     return render_template('index.html',fortune=uranai)
 		
 if __name__ == "__main__":
