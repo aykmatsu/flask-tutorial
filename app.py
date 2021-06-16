b342c7d40245b5e8fb67d6e6194481661c1cdd9c
aykmatsu <toda2020-014@iniad.org>
2021-06-16 02:14:06 +0000

    変更

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = app = Flask(__name__)
 @app.route('/')
 def hello():
     uranai = random.choice(['大吉','中吉','小吉'])
-    return render_template("index.html",fortune=uranai)
+    return render_template('index.html',fortune=uranai)
 		
 if __name__ == "__main__":
     app.run(debug=True)
